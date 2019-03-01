029deac4502a56958faedf654ecd3ec20e86a52c
Evecang <547624569@qq.com>
2019-03-01 11:52:38 +0800

    初始版本

---
+++ application/config/sq_config.php
@@ -0,0 +1,7 @@
+<?php 
+		$sq_base_url='http://localhost/savsoftquiz_v4.0_advance/';
+		$sq_hostname='localhost';
+		$sq_dbname='sqtry2';
+		$sq_dbusername='root';
+		$sq_dbpassword='';
+		?>
---
+++ savsoftquiz_v4_0.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : exam
+ Source Server Type    : MySQL
+ Source Server Version : 100137
+ Source Host           : localhost:3306
+ Source Schema         : savsoftquiz
+
+ Target Server Type    : MySQL
+ Target Server Version : 100137
+ File Encoding         : 65001
+
+ Date: 01/03/2019 11:44:29
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
