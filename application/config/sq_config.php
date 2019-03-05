b940b0007310fa3be14117b20464f0ff6c171177
Evecang <547624569@qq.com>
2019-03-05 16:05:25 +0800

    修改了说明，and重命名了application/controllers/Install.php文件，改为Rename_Install.php

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,7 +1,9 @@
 <?php 
-		$sq_base_url='http://localhost/savsoftquiz_v4.0_advance/';
+		// $sq_base_url='http://localhost/savsoftquiz_v4.0_advance/';
+		$sq_base_url='http://localhost:8081/';
 		$sq_hostname='localhost';
-		$sq_dbname='sqtry2';
+		// $sq_dbname='sqtry2';
+		$sq_dbname='savsoftquiz_v4_0';
 		$sq_dbusername='root';
-		$sq_dbpassword='';
+		$sq_dbpassword=' ';
 		?>
