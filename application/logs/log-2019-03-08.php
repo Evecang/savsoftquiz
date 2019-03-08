c9029c9e00c9a928093e6972f4e94d908b11fa4a
Evecang <547624569@qq.com>
2019-03-08 20:14:53 +0800

    日志

---
+++ application/logs/log-2019-03-08.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-03-08 17:39:17 --> Severity: Warning --> Division by zero D:\XAMPP\htdocs\savsoftquiz\application\views\dashboard.php 231
